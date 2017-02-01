8ac2fb6a15a251fbb3edd28b7c7ba9ae3d5a14d9
Brandon Stilson <bbstilson@fastmail.com>
2017-01-31 22:03:27 -0800

    add redux-devtool

--- client/src/index.js
+++ client/src/index.js
@@ -5,8 +5,10 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import createStore from './redux/create';
 
+const devTool = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
 ReactDOM.render(
-  <Provider store={createStore()}>
+  <Provider store={createStore(devTool)}>
     <Router />
   </Provider>,
   document.getElementById('root')
